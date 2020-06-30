c5a05ea4dac318214e9404e793e1657746a0fa85
Takumi Saito <dev@umeru.ma>
2020-06-30 16:43:04 +0900

    Init

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <span class="home">
+  </span>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Home extends Vue {
+  mounted() {
+    console.log('Home mounted')
+  }
+}
+</script>
+
+<style lang="home">
+/* .component {
+} */
+</style>
